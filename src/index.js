e9c408b3543924616a2d7bd43852400c059560f0
beatriz <beatrizffeo@gmail.com>
2024-03-27 21:52:43 -0600

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,21 +1,57 @@
-// index.js
 
+const detailImg = document.querySelector(".detail-image")
+const detailName = document.querySelector(".name")
+const detailRestaurant = document.querySelector(".restaurant")
+const detailRating = document.querySelector("#rating-display")
+const detailComment = document.querySelector("#comment-display")
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  detailImg.src = ramen.image
+  detailImg.alt = ramen.name
+  detailName.textContent = ramen.name
+  detailRestaurant.textContent = ramen.restaurant
+  detailRating.textContent = ramen.rating
+  detailComment.textContent = ramen.comment
+ 
 };
 
 const addSubmitListener = () => {
-  // Add code
+  const form = document.querySelector("#new-ramen")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const ramen = {
+      name: e.target.name.value, 
+      restaurant: e.target.restaurant.value,
+      image: e.target.image.value,
+      rating: e.target.rating.value,
+      comment: e.target["new-comment"].value,
+    }
+    displayRamen(ramen)
+    e.target.reset()
+  })
+
+}
+const ramenMenu = document.querySelector("#ramen-menu")
+
+const displayRamen= (ramen) =>{
+const img = document.createElement('img')
+img.addEventListener("click", () => handleClick (ramen))
+img.src = ramen.image
+img.alt = ramen.name
+ramenMenu.append(img)
 }
 
-const displayRamens = () => {
-  // Add code
+const displayRamens = async () => {
+  const response = await fetch("http://localhost:3000/ramens")
+  const ramens = await response.json()
+ for(const ramen of ramens) {
+  displayRamen(ramen)
+ }
 };
 
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+displayRamens()
+addSubmitListener()
 }
 
 main()
@@ -27,3 +63,4 @@ export {
   handleClick,
   main,
 };
+
